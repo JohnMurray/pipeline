80a491e7617b99d79ab032338178e509271adefa
John Murray <me@johnmurray.io>
2016-08-28 05:46:41 +0000

    updated notes / makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,11 @@ default: release
 
 setup:
 	npm install -g doctoc
+	cargo install rusty-tags
+	# NOTE: This requires that our .zshrc file contain the the following:
+	# export RUST_SRC_PATH=$HOME/projects/rust-src/
+	git clone https://github.com/rust-lang/rust.git ~/projects/rust-src
+	cd ~/projects/rust-src && git checkout stable
 
 release:
 	doctoc readme.md --github --notitle --maxlevel 3
---
+++ src/run.rs
@@ -0,0 +1,13 @@
+
+/// RunConfiguration
+///   This describes the build definitions or set of phases that a run will
+///   go through. This can generally be thought of as the "configuration" for any given
+///   run and a representation of what is provided in the `pipeline.yaml` file.
+struct RunConfiguration {
+}
+
+/// Run
+///   Represents a "run through" of hte pipeline, meaning an instance in time when
+///   the RunConfiguration was executed.
+struct Run {
+}
